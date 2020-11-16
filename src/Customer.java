9063d8fc90cf46bfc0e7338595b78047e1f84bbd
NedsBastard <58260443+NedsBastard@users.noreply.github.com>
2020-11-16 10:11:18 +0100

    Add files via upload

---
+++ src/Customer.java
@@ -0,0 +1,85 @@
+public class Customer {
+    private  String email;
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private String phoneNumber;
+
+    public Customer() {
+    }
+
+
+    public Customer(String email, String firstName, String lastName, String adress, String city, String zipCode, String state,  String country, String phoneNumber) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.adress = adress;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.state = state;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
